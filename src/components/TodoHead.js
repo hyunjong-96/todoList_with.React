f752886adf8909537ea8eee87ca7f2b21b082667
hyunjong_96 <ahajongs@gmail.com>
2021-01-25 22:24:30 +0900

    #1 TodoTemplate, TodoHead

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import styled from 'styled-components'
 //오늘의 날짜와 요일을 보여주고, 앞으로 해야할 일이 몇개 남았는지 보여줌
+
+const HeadBlock = styled.div`
+    border : 1px solid red;
+    padding : 3rem 2rem;
+
+    h1{
+        margin : 0;
+        font-size:1.5rem;
+        color : #343a40;
+    }
+    .day{
+        margin:4px;
+        font-size : 20px;
+        color : #868e96;
+    }
+    .tasks-left{
+        color : #20c997;
+        margin-top : 1rem;
+        font-weight : bold;
+        font-size:1.2rem;
+    }
+`
+
 function TodoHead(){
-    
+    return(
+        <HeadBlock>
+            <h1>2021년 1월 25일</h1>
+            <div className="day">화요일</div>
+            <div className="tasks-left">할 일 2개 남음</div>
+        </HeadBlock>
+    )
 }
 
 export default TodoHead
--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import styled from 'styled-components'
 //우리가 만들 todoList의 레이아웃을 설정하는 컴포넌트
 //페이지의 중앙에 그림자가 적용된 흰색 박스를 보여줌.
-function TodoTemplate(){
-    
+const TemplateBlock=styled.div`
+    display:flex;
+    border-radius: 16px;
+    flex-direction : column;
+    position : relative;
+
+    margin : 0 auto;
+    margin-top : 96px;
+
+    width : 480px;
+    height : 700px;
+
+    background:white;
+    box-shadow : 0 0 8px 0 rgba(0,0,0,0.04);
+`
+
+function TodoTemplate({children}){
+    return(
+        <TemplateBlock>
+            {children}
+        </TemplateBlock>
+    )
 }
 
 export default TodoTemplate
